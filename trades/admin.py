403afc0ad03d565b8fdd0547a697d860cdfd2ebc
gijshagen <gijshagen@live.nl>
2025-11-16 12:53:04 +0100

    Register Trade model in Django admin

--- trades/admin.py
+++ trades/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Trade
 
-# Register your models here.
+
+@admin.register(Trade)
+class TradeAdmin(admin.ModelAdmin):
+    list_display = (
+        "trade_datetime",
+        "symbol",
+        "side",
+        "quantity",
+        "price",
+        "currency",
+        "broker",
+    )
+    list_filter = ("broker", "symbol", "side", "currency")
+    search_fields = ("symbol", "isin")
+    ordering = ("-trade_datetime",)
