1e514e3223f54977a574571a3f3244c1468a9768
gijshagen <gijshagen@live.nl>
2025-11-22 13:05:19 +0000

    Server: productie config + homepage + deployment setup

--- greenpad_dashboard/settings.py
+++ greenpad_dashboard/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,11 @@ SECRET_KEY = 'django-insecure-ao8lr-0ec%k)02k^j($x^&5&br@7o5e!mvod=cgrc1f^@04!y5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "77.42.18.218",
+    "greenpad-dashboard.nl",
+    "www.greenpad-dashboard.nl",
+]
 
 
 # Application definition
@@ -76,12 +81,15 @@ WSGI_APPLICATION = 'greenpad_dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "greenpad_dashboard",
+        "USER": "greenpad_user",
+        "PASSWORD": "GJJgreenpad56!",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -118,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
